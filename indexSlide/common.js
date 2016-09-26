ab9794de62aac808ce71a7ecc719b348ca94be62
liujianhuanzz <jianhuan0622@csu.edu.cn>
2016-09-26 16:15:46 +0800

    修复文件后缀名错误bug

--- indexSlide/common.js
+++ indexSlide/common.js
@@ -1,6 +1,6 @@
 requirejs.config({
     paths: {
-        jquery: './jquery-1.11.0.min.js',
+        jquery: './jquery-1.11.0.min',
         lazyload: './jquery.lazyload',
         indexSlide:'./indexSlide'
     },
